5944d54d6e35606be79b1fc70528f452bb0604f3
Serhii Chernenko <serhii.chernenko@vaimo.com>
2025-01-27 12:45:55 +0100

    test deploy

--- src/layers/core/data/nuxt.config.ts
+++ src/layers/core/data/nuxt.config.ts
@@ -1,14 +1,14 @@
 export default defineNuxtConfig({
   extends: ['@demo/ui'],
   modules: ['@nuxt/eslint', '@nuxthub/core'],
+  $development: {
+    hub: {
+      remote: true,
+    },
+  },
   devtools: { enabled: true },
   compatibilityDate: '2025-01-27',
   hub: {
     kv: true,
   },
-  $development: {
-    hub: {
-      remote: true
-    }
-  }
 })
